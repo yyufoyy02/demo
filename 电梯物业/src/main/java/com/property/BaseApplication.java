e6a4fe4aa198d88a54d927d22de962648a3293ca
yyufoyy02 <531933208@qq.com>
2015-10-21 15:30:18 +0800

    update

--- 电梯物业/src/main/java/com/property/BaseApplication.java
+++ 电梯物业/src/main/java/com/property/BaseApplication.java
@@ -3,17 +3,70 @@ package com.property;
 import android.app.Application;
 import android.content.Context;
 
+import com.property.activity.R;
+import com.vk.simpleutil.XSimpleBaseUtil;
+import com.vk.simpleutil.XSimpleBaseUtil.Builder;
+import com.vk.simpleutil.library.XSimpleImage;
+import com.vk.simpleutil.library.XSimpleLogger;
+
 /**
- * Created by Heart on 2015/7/16.
+ * @author Administrator
  */
 public class BaseApplication extends Application {
 
+
+    /**
+     * getContext
+     */
     public static Context mContext;
 
-    @Override
+
+    /**
+     * A singleton instance of the application class for easy access in other
+     * places
+     */
+    private static BaseApplication sInstance;
+    public boolean isPad;
+    public boolean debug;
+
     public void onCreate() {
         super.onCreate();
+        sInstance = this;
+        mContext = getApplicationContext();
+        DebugMode(true);
+        DataInit();
+    }
 
+    /**
+     * DebugMode
+     */
+    private void DebugMode(Boolean Debug) {
+        debug = Debug;
+
+        XSimpleLogger.setDebugMode(Debug);// 日志文件调试模式
+    }
+
+    /**
+     * 数据初始化
+     */
+    private void DataInit() {
+
+        XSimpleBaseUtil.initConfig(new Builder().initContext(mContext)
+                .isDebug(debug).isPad(isPad).alertDialogTheme(R.style.MyAlertDialogStyle)
+                .initDefaultRequestParams(null).build());
+        XSimpleImage.initDefaultImageLoaderConfig(getApplicationContext());
+
+    }
+
+
+    /**
+     * synchronized
+     *
+     * @return ApplicationController singleton instance
+     */
+    public static BaseApplication getInstance() {
+
+        return sInstance;
     }
 
 }
