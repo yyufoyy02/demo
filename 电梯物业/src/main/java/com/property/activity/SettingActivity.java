5d919350b9f29526fba5484795db2ad66e34b05c
yyufoyy02 <531933208@qq.com>
2015-10-21 21:26:57 +0800

    update

---
+++ 电梯物业/src/main/java/com/property/activity/SettingActivity.java
@@ -0,0 +1,21 @@
+package com.property.activity;
+
+
+import com.property.base.BaseActivity;
+
+public class SettingActivity extends BaseActivity {
+    @Override
+    public int onCreateViewLayouId() {
+        return R.layout.setting_activity;
+    }
+
+    @Override
+    public void initAllData() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+}
