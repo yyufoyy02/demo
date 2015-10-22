34fcce728c4b4fda09f9c654463eb6387729e7b0
yyufoyy02 <531933208@qq.com>
2015-10-22 18:36:04 +0800

    update 二维码

--- 电梯物业/src/main/java/com/property/activity/MessageActivity.java
+++ 电梯物业/src/main/java/com/property/activity/MessageActivity.java
@@ -4,12 +4,11 @@ import android.content.Intent;
 import android.view.View;
 
 import com.property.base.BaseActivity;
+import com.property.ui.codeScan.CaptureActivity;
+import com.vk.simpleutil.library.XSimpleLogger;
 
 public class MessageActivity extends BaseActivity {
-    public enum MessageType {
-        maintenance, repair, all
-    }
-
+    private final int REQUEST_CODE_SCANLE = 99;
     MessageType type;
 
     @Override
@@ -39,8 +38,22 @@ public class MessageActivity extends BaseActivity {
         findViewById(R.id.toolbar).findViewById(R.id.search).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(mContext, DetailActivity.class));
+                CaptureActivity.launchActivity(mActivity, REQUEST_CODE_SCANLE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SCANLE && resultCode == RESULT_OK) {
+            XSimpleLogger.Log().e("code:" + data.getStringExtra("code"));
+            startActivity(new Intent(mContext, DetailActivity.class));
+        }
+
+    }
+
+    public enum MessageType {
+        maintenance, repair, all
     }
 }
