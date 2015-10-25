d41c13221ac03a2a0d560589631aeb7b4682c2ee
yyufoyy02 <yy84312182>
2015-10-26 01:04:05 +0800

    用户

--- 电梯物业/src/main/java/com/property/activity/DetailActivity.java
+++ 电梯物业/src/main/java/com/property/activity/DetailActivity.java
@@ -2,13 +2,38 @@ package com.property.activity;
 
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.property.base.BaseActivity;
+import com.property.model.ElevatorModel;
+import com.vk.simpleutil.library.XSimpleTime;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.annotation.event.OnClick;
 
 public class DetailActivity extends BaseActivity {
+
+    @InjectView(R.id.edt_detail_id)
+    TextView edtDetailId;
+    @InjectView(R.id.edt_detail_address)
+    TextView edtDetailAddress;
+    @InjectView(R.id.edt_detail_brand)
+    TextView edtDetailBrand;
+    @InjectView(R.id.edt_detail_property_company)
+    TextView edtDetailPropertyCompany;
+    @InjectView(R.id.edt_detail_maintenance_company)
+    TextView edtDetailMaintenanceCompany;
+    @InjectView(R.id.edt_detail_phone)
+    TextView edtDetailPhone;
+    @InjectView(R.id.edt_detail_lasttime)
+    TextView edtDetailLasttime;
+    @InjectView(R.id.edt_detail_thistime)
+    TextView edtDetailThistime;
+    ElevatorModel elevatorModel = new ElevatorModel();
+
     @Override
     public int onCreateViewLayouId() {
         return R.layout.detail_activity;
@@ -17,6 +42,22 @@ public class DetailActivity extends BaseActivity {
     @Override
     public void initAllData() {
         setTitle("电梯信息");
+        elevatorModel.setId("dt20150912");
+        elevatorModel.setAddress("市政府大院2号梯");
+        elevatorModel.setBrand("三菱");
+        elevatorModel.setProperty_company("惠州物业公司");
+        elevatorModel.setMaintenance_company("惠州维保公司");
+        elevatorModel.setPhone("13380123456");
+        elevatorModel.setLast_time(1420819200);
+        elevatorModel.setThis_time(1445270400);
+        edtDetailId.setText(elevatorModel.getId());
+        edtDetailAddress.setText(elevatorModel.getAddress());
+        edtDetailBrand.setText(elevatorModel.getBrand());
+        edtDetailPropertyCompany.setText(elevatorModel.getProperty_company());
+        edtDetailMaintenanceCompany.setText(elevatorModel.getMaintenance_company());
+        edtDetailPhone.setText(elevatorModel.getPhone());
+        edtDetailLasttime.setText(XSimpleTime.getFormatTimeFromTimestamp((long) elevatorModel.getLast_time(), "yyyy-MM-dd"));
+        edtDetailThistime.setText(XSimpleTime.getFormatTimeFromTimestamp((long) elevatorModel.getThis_time(), "yyyy-MM-dd"));
     }
 
     @Override
@@ -25,9 +66,15 @@ public class DetailActivity extends BaseActivity {
     }
 
 
-
     @OnClick(R.id.tv_detail_submit)
     void submit(View view) {
         startActivity(new Intent(mContext, DetailEditActivity.class));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.inject(this);
     }
 }
---
+++ 电梯物业/src/main/java/com/property/model/ElevatorModel.java
@@ -0,0 +1,106 @@
+package com.property.model;
+
+import com.property.base.BaseModel;
+import com.vk.simpleutil.library.XSimpleText;
+
+/**
+ * Created by Administrator on 2015/10/26.
+ */
+public class ElevatorModel extends BaseModel {
+    String id;
+    String address;
+    String brand;
+    String property_company;
+    String maintenance_company;
+    String phone;
+    double last_time;
+    double this_time;
+
+    /**
+     * 电话ID
+     */
+    public String getId() {
+        return XSimpleText.isEmpty(id, "");
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 地址
+     */
+    public String getAddress() {
+        return XSimpleText.isEmpty(address, "");
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * 品牌
+     */
+    public String getBrand() {
+        return XSimpleText.isEmpty(brand, "");
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * 物业公司
+     */
+    public String getProperty_company() {
+        return XSimpleText.isEmpty(property_company, "");
+    }
+
+    public void setProperty_company(String property_company) {
+        this.property_company = property_company;
+    }
+
+    /**
+     * 维保公司
+     */
+    public String getMaintenance_company() {
+        return XSimpleText.isEmpty(maintenance_company, "");
+    }
+
+    public void setMaintenance_company(String maintenance_company) {
+        this.maintenance_company = maintenance_company;
+    }
+
+    /**
+     * 电话
+     */
+    public String getPhone() {
+        return XSimpleText.isEmpty(phone, "");
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * 上次维保时间
+     */
+    public double getLast_time() {
+        return last_time;
+    }
+
+    public void setLast_time(double last_time) {
+        this.last_time = last_time;
+    }
+
+    /**
+     * 这次维保时间
+     */
+    public double getThis_time() {
+        return this_time;
+    }
+
+    public void setThis_time(double this_time) {
+        this.this_time = this_time;
+    }
+}
